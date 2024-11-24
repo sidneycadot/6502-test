c200d3b230998fe1a8b0078035e36600c6147ea8
sidney <sidney@jigsaw.nl>
2024-11-24 10:21:04 +0100

    Added routine to measure cycles with saving of the zero page.

---
+++ measure_cycles_zp_safe.s
@@ -0,0 +1,44 @@
+
+                .import _measure_cycles
+
+                .bss
+
+SAVE_ZEROPAGE:  .res    256
+
+                .code
+
+_measure_cycles_zp_safe:
+
+                ;
+
+                pha                                     ; Save A and X (test-code address pointer),
+                txa
+                pha
+
+                ldx     #0                              ; Copy zero page to SAVE_ZEROPAGE.
+@saveloop:      lda     0,x
+                sta     SAVE_ZEROPAGE,x
+                inx
+                bne     @saveloop
+
+                pla                                     ; Restore A and X (test-code address pointer).
+                tax
+                pla
+
+                jsr     _measure_cycles
+
+                pha                                     ; Save the A and X return value (cycle count).
+                txa
+                pha
+
+                ldx     #0                              ; Copy SAVE_ZEROPAGE back to zero page.
+@restore_loop:  lda     SAVE_ZEROPAGE,x
+                sta     0,x
+                inx
+                bne     @restore_loop
+
+                pla                                     ; Restore A and X resturn value (cycle count).
+                tax
+                pla
+
+                rts
