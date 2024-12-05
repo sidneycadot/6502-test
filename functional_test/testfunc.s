e6bdc52799b326b26b91f59172617bbeefc6267b
sidney <sidney@jigsaw.nl>
2024-12-05 07:44:53 +0100

    Test version tests for adc/sbc tests without the file interaction.

--- functional_test/testfunc.s
+++ functional_test/testfunc.s
@@ -1,24 +1,26 @@
 
-            .export _BEFORE_P
-            .export _BEFORE_A
+            .export _A_VALUE
+            .export _P_VALUE
             .export _TABLE
+            .export _testfunc
 
-_BEFORE_P   := BEFORE_P
-_BEFORE_A   := BEFORE_A
+_testfunc   := testfunc
+_A_VALUE    := A_VALUE
+_P_VALUE    := P_VALUE
 _TABLE      := TABLE
 
 PTR         := $CD
 
             .bss
 
-BEFORE_P    .res    1
-BEFORE_A    .res    1
-OPERAND     .res    1
-TABLE       .res    1024
+A_VALUE:    .res    1       ; Value transferred to A before the ADC/SBC.
+P_VALUE:    .res    1       ; Value transferred to P before the ADC/SBC  (bits 4 and 5 are ignored).
+OPERAND:    .res    1
+TABLE:      .res    1024    ; We need 256 * 4 bytes to store results: 256 operand values * {ADC A/P, SBC A/P}.
 
             .code
-_testfunc:
 
+testfunc:
             lda     #<TABLE
             sta     PTR
             lda     #>TABLE
@@ -26,12 +28,20 @@ _testfunc:
 
             lda     #0
             sta     OPERAND
-@loop
-            lda     BEFORE_P
+@loop:
+            ; Save old status bits (for the I flag, mostly).
+
+            php
+
+            ; Do ADC test.
+
+            lda     P_VALUE
             pha
-            lda     BEFORE_A
+            lda     A_VALUE
             plp
+
             adc     OPERAND
+
             php
             ldy     #0
             sta     (PTR),y             ; Store ADC A result
@@ -39,17 +49,38 @@ _testfunc:
             ldy     #1
             sta     (PTR),y             ; Store ADC P resu;t
 
-            lda     BEFORE_P
+            ; Do SBC test
+
+            lda     P_VALUE
             pha
-            lda     BEFORE_A
+            lda     A_VALUE
             plp
+
             sbc     OPERAND
+
             php
             ldy     #2
             sta     (PTR),y             ; Store SBC A result
             pla
             ldy     #3
             sta     (PTR),y             ; Store SBC P result
+
+            ; Restore old status bits (for the I flag).
+
+            plp
+
+            ; Increment PTR by 4.
+
+            clc
+            lda     PTR
+            adc     #<4
+            sta     PTR
+            lda     PTR+1
+            adc     #>4
+            sta     PTR+1
+
+            ; Proceed to next OPERAND value.
+
             inc     OPERAND
             bne     @loop
 
