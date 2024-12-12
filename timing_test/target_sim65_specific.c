d08166e50899c9d1026ffabd3d4b8e751222bb1f
Sidney Cadot <sidney@jigsaw.nl>
2024-12-12 10:25:01 +0100

    Fixed sim65 target.

--- timing_test/target_sim65_specific.c
+++ timing_test/target_sim65_specific.c
@@ -1,7 +1,7 @@
 
-//////////////////////
-// target_sim6502.c //
-//////////////////////
+/////////////////////////////
+// target_sim65_specific.c //
+/////////////////////////////
 
 #include <stdio.h>
 #include <stdbool.h>
@@ -28,7 +28,7 @@ void post_big_measurement_block_hook(void)
 
 void pre_every_test_hook(const char * test_description)
 {
-    printf("Test: %s ...\n", test_description);
+    (void)test_description;
 }
 
 bool post_every_measurement_hook(const char * test_description, bool success, unsigned long test_count, unsigned long error_count)
@@ -37,13 +37,6 @@ bool post_every_measurement_hook(const char * test_description, bool success, un
     (void)success;
     (void)test_count;
     (void)error_count;
-
-    return true;
-}
-
-bool zp_address_is_safe(uint8_t address)
-{
-    (void)address;
     return true;
 }
 
@@ -52,4 +45,10 @@ uint8_t * set_irq_vector_address(uint8_t * newvec)
     uint8_t * oldvec = (uint8_t *)PEEKW(0xfffe);
     POKEW(0xfffe, (uint16_t)newvec);
     return oldvec;
+}
+
+bool zp_address_is_safe(uint8_t zp_address)
+{
+    (void)zp_address;
+    return true;
 }
